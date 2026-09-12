873d3b3a3c27b140b3f0455b03b33a9abe2f253d
PhoenixBrehm <phoenixbrehm2700@gmail.com>
2026-09-12 07:49:53 -0500

    Named variables privately, added get methods and member var to track remainingflags

--- board.py
+++ board.py
@@ -3,9 +3,9 @@ class Board:
 
 
     def __init__(self, rows: int = 10, cols: int = 10, numMines = -1):
-        self.rows = rows
-        self.cols = cols
-        self.numMines = numMines #must be user supplied from [10, 20], if not set as -1
+        self.__rows = rows
+        self.__cols = cols
+        self.__numMines = numMines #must be user supplied from [10, 20], if not set as -1
 
         """
         Board Cell State Definitions:
@@ -15,46 +15,58 @@ class Board:
         3 = Unchecked mine
         4 = Correctly flagged mine
         """
-        self.board = [[0] * self.rows] * self.cols #populate board with 0's
-        self.gameState = 0 #0 is neither win or lose, -1 is lose, 1 is win
+        self.__board = [[0] * self.__rows] * self.__cols #populate board with 0's
+        self.__gameState = 0 #0 is neither win or lose, -1 is lose, 1 is win
+        self.__remainingFlags = numMines
 
     def populateBoard(self, clickedCellRow, clickedCellCol):
         #row is each set of lists
-        for i in range(self.numMines):
-            rowMine = random.randint(0, self.rows-1)
-            colMine = random.randint(0, self.cols-1)
-            if(rowMine == clickedCellRow and colMine == clickedCellCol and self.board[rowMine][colMine] == 3):
+        for i in range(self.__numMines):
+            rowMine = random.randint(0, self.__rows-1)
+            colMine = random.randint(0, self.__cols-1)
+            if(rowMine == clickedCellRow and colMine == clickedCellCol and self.__board[rowMine][colMine] == 3):
                 i = i - 1
             else:
-                self.board[rowMine][colMine] = 3
+                self.__board[rowMine][colMine] = 3
 
     def setState(self, clickedCellRow, clickedCelCol, isFlagging = False): # should be t/f
         if(isFlagging): #if right clicking, handle removing flags on 1 & 4, add flags on 3 and 0, ignore 2's
-            if(self.board[clickedCellRow][clickedCelCol] == 3):
-                self.board[clickedCellRow][clickedCelCol] = 4
-            elif(self.board[clickedCellRow][clickedCelCol] == 1):
-                self.board[clickedCellRow][clickedCelCol] = 0
-            elif(self.board[clickedCellRow][clickedCelCol] == 4):
-                self.board[clickedCellRow][clickedCelCol] = 3
-            elif(self.board[clickedCellRow][clickedCelCol] == 0):
-                self.board[clickedCellRow][clickedCelCol] = 1
+            if(self.__board[clickedCellRow][clickedCelCol] == 3):
+                self.__board[clickedCellRow][clickedCelCol] = 4
+                self.__remainingFlags -= 1
+            elif(self.__board[clickedCellRow][clickedCelCol] == 4):
+                self.__board[clickedCellRow][clickedCelCol] = 3
+                self.__remainingFlags += 1
+            elif(self.__board[clickedCellRow][clickedCelCol] == 1):
+                self.__board[clickedCellRow][clickedCelCol] = 0
+                self.__remainingFlags += 1
+            elif(self.__board[clickedCellRow][clickedCelCol] == 0):
+                self.__board[clickedCellRow][clickedCelCol] = 1
+                self.__remainingFlags -= 1
         else: #if left clicking, only handle clicking on 0 or 3, ignore clicks on 1 2 4
-            if(self.board[clickedCellRow][clickedCelCol] == 3):
-                self.gameState = -1
-            elif(self.board[clickedCellRow][clickedCelCol] == 0):
+            if(self.__board[clickedCellRow][clickedCelCol] == 3):
+                self.__gameState = -1
+            elif(self.__board[clickedCellRow][clickedCelCol] == 0):
                 pass
 
     def hasWon(self):
-        if(self.gameState == -1):
+        if(self.__gameState == -1):
             return -1
 
-        for i in range(self.rows):
-            for j in range(self.cols):
-                if(self.board[i][j] == 0 or self.board[i][j] == 1):
-                    self.gameState = 0
+        for i in range(self.__rows):
+            for j in range(self.__cols):
+                if(self.__board[i][j] == 0 or self.__board[i][j] == 1):
+                    self.__gameState = 0
                     return 0
         
-        self.gameState = 1
+        self.__gameState = 1
         return 1
 
-    
+    def getRows(self):
+        return self.__rows
+    def getCols(self):
+        return self.__cols
+    def getCellState(self, row, col):
+        return self.__board[row][col]
+    def getNumRemainingFlags(self):
+        return self.__remainingFlags
