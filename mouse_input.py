ce2f73ff310dd8ca36b4066eb23cc9f7490b120b
Maha Jornaz <mahajornaz1@gmail.com>
2026-09-14 17:47:01 -0500

    Implement mouse input handling with Tkinter
    Added a GUI with buttons to capture mouse clicks.

--- mouse_input.py
+++ mouse_input.py
@@ -1,1 +1,18 @@
 
+import tkinter
+
+
+def clicked_cell(row, column):
+    selection = (row, column)      #used to debug/test
+    print(selection)
+    
+
+window = tkinter.Tk()
+
+for x in range(0, 10):
+    for y in range(0, 10):
+        button = tkinter.Button(window, command=lambda r = x, c = y: clicked_cell(r, c))  
+        button.grid(row = x, column = y)
+
+
+window.mainloop()
