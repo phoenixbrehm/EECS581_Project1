a2fb424f611de8596dfe854988e6440f5f0bd0e4
Ethan Le <e074l430@ku.edu>
2026-09-14 19:43:33 -0500

    Added new folder images that can be used if want to add any of the minesweeper images. Added comments into the mouse_input file for clarity and added some other things for testing for "potential" use.

--- mouse_input.py
+++ mouse_input.py
@@ -1,18 +1,45 @@
+# tkinter for the buttons and images.
+import tkinter as tk
+# PIL for image resizing.
+from PIL import Image, ImageTk
 
-import tkinter
+# Makes the window for the grid at the bottom two for loop.
+window = tk.Tk()
 
+""" 
+Grabs the image from the image folder and resises it to match the size of the buttons. (test with other images if wanted like bombs).
+Image must be resized because it would scale the button the image size instead.
+button_image makes use of the resized image.
+blank_image is used to make the buttons blank at the start of the game, but also makes it so the width and height parameters are in pixels.
+"""
+original_image = Image.open("EECS581_Project1/images/MINESWEEPER_FLAG.png")
+resized_image = original_image.resize((32, 32), Image.Resampling.NEAREST)
+button_image = ImageTk.PhotoImage(resized_image)
+blank_image = tk.PhotoImage(width=1, height=1)
 
+# Maha's clicked_cell function.
 def clicked_cell(row, column):
+    # Gets the row and column of the button and then prints it in the terminal.
     selection = (row, column)      #used to debug/test
     print(selection)
-    
 
-window = tkinter.Tk()
+# Left click function that changes the color of the background of the button to light gray.
+def left_click(event):
+    event.widget.configure(bg="light gray")
 
+# Right click function that changes the image of the button to the flag image.
+def right_click(event):
+    event.widget.configure(image=button_image)
+    event.widget.image = button_image
+
+# Maha's double for loop for creating the blank button grid.
 for x in range(0, 10):
     for y in range(0, 10):
-        button = tkinter.Button(window, command=lambda r = x, c = y: clicked_cell(r, c))  
+        button = tk.Button(window, image=blank_image, width=32, height=32, command=lambda r = x, c = y: clicked_cell(r, c))  
         button.grid(row = x, column = y)
+        # Ethan Le's left and right click functions that are binded to the buttons.
+        button.bind("<Button-1>", left_click)
+        button.bind("<Button-3>", right_click)
 
-
+# Lets you press the buttons as many times as you want until you close the window.
 window.mainloop()
