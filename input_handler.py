21ba658fe81bef70319b0eee14332064b9a1395e
PhoenixBrehm <143414798+phoenixbrehm@users.noreply.github.com>
2026-09-14 20:04:49 -0500

    Update to make return types consistent and added selection of mode

--- input_handler.py
+++ input_handler.py
@@ -1,32 +1,40 @@
 
 def get_input():
-    user_input = input("Enter a cell: ").upper().strip()
+    user_input_cell_location = input("Enter a cell: ").upper().strip()
+    user_input_click_mode = input("Enter L (for left clicking) or R (for right clicking): ").upper().strip()
 
     col_letters = "ABCDEFGHIJ"
 
-    if len(user_input) < 2:
+    if len(user_input_cell_location) < 2:
         print("Error: Empty input")
-        return False
+        return (-1, -1, False)
 
 
-    if user_input[0] not in col_letters:
+    if user_input_cell_location[0] not in col_letters:
         print("Error: Invalid column or order")
-        return False
+        return (-1, -1, False)
 
 
-    if not user_input[1:].isdigit():
+    if not user_input_cell_location[1:].isdigit():
         print("Error: Non-number row")
-        return False
+        return (-1, -1, False)
 
+    if user_input_click_mode != "L" and user_input_click_mode != "R":
+        print("Error: No proper click mode specified")
+        return (-1, -1, False)
     
-    row = int(user_input[1:]) - 1
+    row = int(user_input_cell_location[1:]) - 1
     if row < 0 or row > 9:
         print("Error: Row out of bounds")
-        return False
+        return (-1, -1, False)
     
     
-    column = col_letters.index (user_input[0])
-    return (row, column)
+    column = col_letters.index (user_input_cell_location[0])
+    if user_input_click_mode == "L":
+        mode = False
+    if user_input_click_mode == "R":
+        mode = True
+    return (row, column, mode)
 
 
 #selection = (get_input())      [used to debug/test]
